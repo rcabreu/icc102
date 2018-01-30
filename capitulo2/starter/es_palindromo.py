14a2c36bc5910f03114b4b63308a6b1d93673081
Roberto Abreu <rcabreu@users.noreply.github.com>
2018-01-30 12:12:00 -0400

    Adding es_palindromo

---
+++ capitulo2/starter/es_palindromo.py
@@ -0,0 +1,15 @@
+import sys
+
+def es_palindromo(str):
+    # devuelve True si es palindromo
+    return False
+
+def main():
+    str = input()
+
+    ans = es_palindromo(str)
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
