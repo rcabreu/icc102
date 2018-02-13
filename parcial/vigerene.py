a9f89103415c8763f2204e742df79ee7e0edbf3d
Roberto Abreu <rcabreu@users.noreply.github.com>
2018-02-13 10:15:51 -0400

    Create vigerene.py

---
+++ parcial/vigerene.py
@@ -0,0 +1,34 @@
+import sys
+
+ALFA = 'abcdefghijklmnopqrstuvwxyz'
+def encriptar(msg, llave):
+  res = ''
+  i = 0
+  j = 0
+  while i < len(msg):
+    chM = msg[i]
+    chL = llave[j]
+    print(chM)
+    print('-')
+    print(i)
+    idx = int(ALFA.index(chM))
+    idx += int(ALFA.index(chL))
+    res += ALFA[idx % 26]
+    i += 1
+    j += 1
+    if j == len(llave):
+      j = 0
+  return res
+
+def desencriptar(cifrado, llave):
+  return 'todavia no implementado'
+
+def main():
+  llave = 'aosksow'
+  print('digita la cadena que quieres encriptar')
+  mensaje = input()
+  cifrado = encriptar(mensaje, llave)
+  print('resultado: ' + cifrado)
+  print('mensaje original: ' + desencriptar(cifrado, llave))
+  
+main()
